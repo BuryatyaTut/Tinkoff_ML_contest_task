7489f267ff9f78411cd457ef8e889a6b4b30178a
asad <nonimy542@mail.ru>
2022-09-03 02:39:19 +0300

    traing got more OOP code style

--- solution/train.py
+++ solution/train.py
@@ -2,27 +2,34 @@ import argparse
 import sys
 import re
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--input-dir", help="directory of text files")
-parser.add_argument("--model", help="directory for model to be saved in")
-args = parser.parse_args()
 
-if not args.model:
-    print('Please, enter the path for model to save')
-    exit()
+def get_text():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--input-dir", help="directory of text files")
+    parser.add_argument("--model", help="directory for model to be saved in")
+    args = parser.parse_args()
 
-text = ''
-if args.input_dir:
-    text = open(args.input_dir).read()
-else:
-    print('You dont add path to text files, so type it in stdin.\nWhen you done just print \"eNd_type.\" in a new line')
-    for line in sys.stdin:
-        if line.rstrip() == 'eNd_type.':
-            break
-        text += line
+    if not args.model:
+        print('Please, enter the path for model to save')
+        exit()
 
-# -------------------------- data preprocessing and tokenizing
+    text = ''
+    if args.input_dir:
+        text = open(args.input_dir).read()
+    else:
+        print('You dont add path to text files, so type it in stdin.\nWhen you done just print \"eNd_type.\" in a new '
+              'line')
+        for line in sys.stdin:
+            if line.rstrip() == 'eNd_type.':
+                break
+            text += line
+    return text
 
-text = text.lower()
-tokens = re.split("[^a-zа-я]+", text)
 
+def text_to_tokens(text):
+    text = text.lower()
+    return re.split("[^a-zа-я]+", text)
+
+
+rawText = get_text()
+tokens = text_to_tokens(rawText)
