5d911831f14518d49f9ab943c0d365dfeb693705
asad <nonimy542@mail.ru>
2022-09-03 02:47:23 +0300

    generate got more OOP code style

--- solution/generate.py
+++ solution/generate.py
@@ -1,16 +1,24 @@
 import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--model", help="enter the path where model located")
-parser.add_argument("--prefix", help="start of sentence: one or more words")
-parser.add_argument("--length", help="length of generating model")
-args = parser.parse_args()
 
-if not args.model:
-    print("Please, enter path where model located")
-    exit()
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", help="enter the path where model located")
+    parser.add_argument("--prefix", help="start of sentence: one or more words")
+    parser.add_argument("--length", help="length of generating model")
+    args = parser.parse_args()
 
-length = 10
-if args.length:
-    length = args.length
+    if not args.model:
+        print("Please, enter path where model located")
+        exit()
+
+    length = 10
+    if args.length:
+        length = args.length
+
+    prefix = ''  # something random
+    if args.prefix:
+        prefix = args.prefix
+
+    return args.model, length, prefix
 
